a834462d50ffcc6a83d555f1b8ad2695ccf8a0b1
OXOO1337 <60068297+OXOO1337@users.noreply.github.com>
2020-02-18 20:41:09 +0300

    Update Anaconda.sh

--- Anaconda.sh
+++ Anaconda.sh
@@ -1,16 +1,8 @@
 #!/bin/bash
 # sudo chmod +x Anaconda.sh
 ##################################
-# source .bashrc
-# anaconda-navigator
-# source deactivate
-# __________________________________
-# !/bin/bash
-# source /home/$USERNAME/anaconda3/bin/activate
-# anaconda-navigator
-# ___________________________________
-# conda config --set auto_activate_base False
-#####################################
+# Debian - ubuntu - kali linux
+##################################
 #Colors
 white="\033[1;37m"
 grey="\033[0;37m"
@@ -31,36 +23,41 @@ clear
 # display menu
 echo -e $transparent"PC Name - $red$(hostname)$transparent"
 echo "--------------------"
-echo -e "|" $white"INSTALL"$blue" Anaconda"$transparent" |"
+echo -e "|   "$blue" Anaconda3"$transparent"     |"
 echo "--------------------"
-echo -e $yellow"["$red"0"$yellow"]"$red" Exit"
-echo -e $red"["$yellow"1"$red"]"$transparent" Update"
-echo -e $red"["$blue"2"$red"]"$transparent" INSTALL Anaconda"
-echo -e $red"["$yellow"3"$red"]"$transparent" run Anaconda"
-echo -e $red"["$blue"4"$red"]"$transparent" No more (base)"
-echo -e $red"["$yellow"5"$red"]"$transparent" source deactivate"
+echo -e $yellow"["$red"0"$yellow"]"$red" Exit$transparent"
+echo -e $red"["$yellow"1"$red"]"$transparent" CP Update"
+echo -e $red"["$blue"2"$red"]"$transparent" INSTALL Anaconda3"
+echo -e $red"["$yellow"3"$red"]"$transparent" Run Anaconda"
+echo -e $red"["$blue"4"$red"]"$transparent" Exit (base)"
+echo -e $red"["$yellow"5"$red"]"$transparent" No more (base)"
+echo -e $red"["$blue"6"$red"]"$transparent"$red Remove Anaconda3$transparent"
 # get-e input from the user
-echo -e $blue "Enter your choice $yellow[$red 0 $white-$yellow 4 ] $transparent" $green ; read -p  "Number: " choice
+echo -e $blue "Enter your choice $yellow[$red 0 $white-$yellow 6 ] $transparent" $green ; read -p  "Number: " choice
 # make decision using case..in..esac
  case $choice in
+   #CP Update
 1)
 sudo apt-get update&&apt-get upgrade -y
 echo -e $yellow"Done"$transparent
 read -p "Press [Enter] key to continue..."
 readEnterKey
 ;;
+   #INSTALL Anaconda3
 2)
 sudo apt-get install libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 curl -y
 cd /tmp && curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh;
-bash ~/tmp/Anaconda3-2019.10-Linux-x86_64.sh;
+bash Anaconda3-2019.10-Linux-x86_64.sh;
 source ~/.bashrc;
 conda info;
 conda update conda;
 conda update anaconda;
+sudo rm /tmp/Anaconda3-2019.10-Linux-x86_64.sh;
 echo -e $yellow"Done"$transparent
 read -p "Press [Enter] key to continue..."
 readEnterKey
 ;;
+   #Run Anaconda
 3)
 source /home/$USERNAME/anaconda3/bin/activate
 anaconda-navigator
@@ -68,14 +65,24 @@ echo -e $yellow"Done"$transparent
 read -p "Press [Enter] key to continue..."
 readEnterKey
 ;;
+   #Exit (base)
 4)
+source deactivate
+echo -e $yellow"Done"$transparent
+read -p "Press [Enter] key to continue..."
+readEnterKey
+;;
+   #No more (base)
+5)
 conda config --set auto_activate_base False
 echo -e $yellow"Done"$transparent
 read -p "Press [Enter] key to continue..."
 readEnterKey
 ;;
-5)
-source deactivate
+   #Remove Anaconda
+6)
+sudo rm -rf ~/anaconda3;
+sudo rm -rf ~/.condarc ~/.conda ~/.continuum;
 echo -e $yellow"Done"$transparent
 read -p "Press [Enter] key to continue..."
 readEnterKey
